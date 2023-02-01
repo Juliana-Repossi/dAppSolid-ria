907c4e8e356af68d3b1f72b70de3d1fd0969a0e5
Juliana Repossi <julianarepossi@hotmail.com>
2023-02-01 16:59:49 -0300

    Conjunto home finalizado e revisado

--- Pages/Home/home.js
+++ Pages/Home/home.js
@@ -1,94 +1,9 @@
-// 1. Declare global variable to store the smart contract instance
+// instancia do contrato
 let SoliContract;
 
-// 2. Set contract address and ABI
-const Soli_Contract_Address = "0xa698722a18b46f6999ac6bd682c584022b3c6ab5";
+// endereço do contrato e ABI para instanciar o contrato
+const Soli_Contract_Address = "0x7267Ff37a2fbf5E9884c48ED121F9CA38Fe2f420";
 const Soli_Contract_ABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "comprador",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "vendedor",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "item",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "valor",
-				"type": "uint256"
-			}
-		],
-		"name": "compradorCancelaCompra",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "comprador",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "vendedor",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "valor",
-				"type": "uint256"
-			}
-		],
-		"name": "compradorRecebeCompra",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "comprador",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "vendedor",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "item",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "valor",
-				"type": "uint256"
-			}
-		],
-		"name": "criaCompra",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
@@ -120,6 +35,12 @@ const Soli_Contract_ABI = [
 				"internalType": "string",
 				"name": "comprador",
 				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "situacao",
+				"type": "bool"
 			}
 		],
 		"name": "notificaVenda",
@@ -176,36 +97,16 @@ const Soli_Contract_ABI = [
 				"internalType": "string",
 				"name": "item",
 				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "preco",
+				"type": "uint256"
 			}
 		],
 		"name": "pedidoEnviado",
 		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "codinome",
-				"type": "string"
-			}
-		],
-		"name": "recompensaCadastro",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "codinome",
-				"type": "string"
-			}
-		],
-		"name": "recompensaOfertaItem",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
 	},
 	{
 		"anonymous": false,
@@ -213,7 +114,38 @@ const Soli_Contract_ABI = [
 			{
 				"indexed": false,
 				"internalType": "string",
+				"name": "proprietario",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "saldo",
+				"type": "uint256"
+			}
+		],
+		"name": "saldoAlterado",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "comprador",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
 				"name": "vendedor",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "item",
 				"type": "string"
 			},
 			{
@@ -225,6 +157,25 @@ const Soli_Contract_ABI = [
 		],
 		"name": "valorLiberadoVendedor",
 		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "codinome",
+				"type": "string"
+			}
+		],
+		"name": "SaldoLivreCodinome",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"inputs": [
@@ -242,9 +193,42 @@ const Soli_Contract_ABI = [
 				"internalType": "string",
 				"name": "item",
 				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "valor",
+				"type": "uint256"
 			}
 		],
-		"name": "vendedorEnviaCompra",
+		"name": "compradorCancelaCompra",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "comprador",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "vendedor",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "item",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "preco",
+				"type": "uint256"
+			}
+		],
+		"name": "compradorRecebeCompra",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
@@ -277,6 +261,60 @@ const Soli_Contract_ABI = [
 		"inputs": [
 			{
 				"internalType": "string",
+				"name": "comprador",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "vendedor",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "item",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "valor",
+				"type": "uint256"
+			}
+		],
+		"name": "criaCompra",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "codinome",
+				"type": "string"
+			}
+		],
+		"name": "recompensaCadastro",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "codinome",
+				"type": "string"
+			}
+		],
+		"name": "recompensaOfertaItem",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
 				"name": "",
 				"type": "string"
 			},
@@ -287,44 +325,6 @@ const Soli_Contract_ABI = [
 			}
 		],
 		"name": "saldo",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "codinome",
-				"type": "string"
-			}
-		],
-		"name": "SaldoBlockCodinome",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "codinome",
-				"type": "string"
-			}
-		],
-		"name": "SaldoLivreCodinome",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -353,31 +353,66 @@ const Soli_Contract_ABI = [
 		],
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "comprador",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "vendedor",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "item",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "preco",
+				"type": "uint256"
+			}
+		],
+		"name": "vendedorEnviaCompra",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	}
 ]
 
-/* 3. Prompt user to sign in to MetaMask */
-const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
-provider.send("eth_requestAccounts", []).then(() => {
-  provider.listAccounts().then((accounts) => {
-    const signer = provider.getSigner(accounts[0]);
+var itens = [];
 
-    /* 3.1 Create instance of Turing smart contract */
-    SoliContract = new ethers.Contract(
-        Soli_Contract_Address,
-        Soli_Contract_ABI,
-        signer
-    );
-  });
-});
+window.onload = async () => {
 
-itens = [];
+	const usuarioLogado = localStorage.getItem("usuario");
 
+	/* 3. Prompt user to sign in to MetaMask */
+	const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+	provider.send("eth_requestAccounts", []).then(() => {
+	provider.listAccounts().then(async(accounts) => {
+		const signer = provider.getSigner(accounts[0]);
 
-window.onload = function listaTabela() {
- 
+		/* 3.1 Create instance of Turing smart contract */
+		SoliContract = new ethers.Contract(
+			Soli_Contract_Address,
+			Soli_Contract_ABI,
+			signer
+		);
 
-    let tabela = document.getElementById("tabela");
+	//eventos que podem ser ouvidos nessa pag
+	atualizaSaldo(usuarioLogado);
+
+	//exibir o saldo do usuario logado
+	exibeSaldo(usuarioLogado);
+
+	});
+	});
+
+	let tabela = document.getElementById("tabela");
 
     //adicionar produto na lista
     if (localStorage.listaItens)
@@ -394,15 +429,40 @@ window.onload = function listaTabela() {
         let td_descricao = tr.insertCell();
         let td_tipo = tr.insertCell();
         let td_preco = tr.insertCell();
+		let td_vendedor = tr.insertCell();
 
         td_id.innerText = i+1;
         td_nome.innerText = itens[i].nome;
         td_descricao.innerText = itens[i].descricao;
         td_tipo.innerText = itens[i].tipo;
         td_preco.innerText = itens[i].preco;
-
+		td_vendedor.innerText = itens[i].vendedor;
     }
+
 }
+
+
+window.atualizaSaldo = async (usuarioLogado) => {
+	SoliContract.on("saldoAlterado",(codinome,saldo)=>{
+		//verifica se foi o seu saldo que mudou
+		if(usuarioLogado == codinome)
+		{
+			let saldoAtual = document.getElementById("ExibeSaldo");
+			saldoAtual.innerText = saldo;
+		}
+	})
+}
+
+window.exibeSaldo = (usuarioLogado) => {
+	SoliContract.SaldoLivreCodinome(usuarioLogado).then((saldo) =>{
+		let meuSaldo = document.getElementById("ExibeSaldo");
+		meuSaldo.innerText = saldo;
+	})
+	.catch((err) =>{
+		alert(err);
+	})
+}
+
 
 const botaoSolicitar = document.querySelector("#solicitar");
 
@@ -416,35 +476,13 @@ const SolicitaItem = () => {
 		localStorage.setItem("item",nItem-1); 
 
 		//direciona para a pag home
-		window.location.href = "../Item/item.html"
+		window.location.href = "../Carrinho/carrinho.html"
     }
     else
     {
         alert("Produto inválido");
+		itemEscolhido.value = "";
     }
 }
 
 botaoSolicitar.addEventListener("click",SolicitaItem);
-
-
-const botaoSaldo = document.querySelector("#VerSaldo");
-
-const exibeSaldo = () => {
-
-	//usuario logado
-	usr = localStorage.getItem("usuario");
-
-    SoliContract.SaldoLivreCodinome(usr).then((res) => {
-		
-		let saldoAtual = document.getElementById("ExibeSaldo");
-		
-		saldoAtual.innerText = res;
-	})
-	.catch((err) => {
-	// If error occurs, display error message
-		alert(err);
-	});        
-} 
-
-
-botaoSaldo.addEventListener("click", exibeSaldo);
